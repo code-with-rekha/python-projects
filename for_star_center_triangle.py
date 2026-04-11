22ff66efa4d9d4c35e8df046edf0f2246ac8f2ee
Rekha Bhutiya <rekha.bhutiya@yahoo.in>
2026-04-12 00:42:05 +0530

    comment added

---
+++ for_star_center_triangle.py
@@ -0,0 +1,9 @@
+#This is comment for info
+
+for i in range(1,6,2):
+    for j in range(1,6):
+        if j<=i+1:
+            print(" ",end="")
+        if j==i:
+             print("*", end="")
+    print("\n")
